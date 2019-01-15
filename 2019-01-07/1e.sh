d3592f04357e60ea0ab12ffb5276594e3df03672
emre-bahnhof <emre163@live.se>
2019-01-15 12:33:23 +0100

    1f

--- 2019-01-07/1e.sh
+++ 2019-01-07/1e.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo -n "enter a starting value: "
-read -e val
-for ((i = $val ; i > 0; i--)); do #loop val to 0
+for ((i = 10; i > 0; i--)); do #loop 10-1
 	echo -n "$i "
 done
-printf "\n"
+printf "\n" #newline after done
 exit
