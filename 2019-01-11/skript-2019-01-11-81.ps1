eb789f69ddcf0634d833e5d2887607e887d1fb36
unknown <Administrator@VT17_5.LOCAL>
2019-01-25 12:19:54 +0100

    hej

---
+++ 2019-01-11/skript-2019-01-11-81.ps1
@@ -0,0 +1,17 @@
+﻿$FILEPATH = "C:\Users\emre1\OneDrive\Dokument"
+
+$content = get-content $FILEPATH
+
+$workaddress = @'
+_................_
+påhittadevägen 3
+_................_
+'@
+
+$myname = "Emre Celik"
+#echo $myname | gm 
+
+echo $myname.ToUpper()
+echo $content
+
+echo $workaddress
